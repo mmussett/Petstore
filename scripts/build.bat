a6d7d22bcd1bcb4108a783e8d8e3996099b532af
mmussett <mmussett@tibco.com>
2018-09-18 10:35:31 +0000

    Fixed scripts to be relative pathed

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,2 +1,2 @@
-cd C:\dev\Petstore\petstore_sample_service_application.parent
+cd ..\petstore_sample_service_application.parent
 mvn clean package
--- scripts/push.bat
+++ scripts/push.bat
@@ -1,4 +1,4 @@
-cd C:\dev\Petstore\petstore_sample_service_application\target
-copy C:\dev\Petstore\petstore_sample_service_application.parent\manifest.json .
+cd ..\petstore_sample_service_application\target
+copy ..\..\petstore_sample_service_application.parent\manifest.json .
 tibcli login -p T1bc0.Sarah72 -o "EMEA North PreSales" -r "eu-west-1"
 tibcli app push
