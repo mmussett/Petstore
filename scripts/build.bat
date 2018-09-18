b4188381639bf3f41c485120222e3b398381829c
mmussett <mmussett@tibco.com>
2018-09-18 10:26:45 +0000

    Added scripts for build/deploy

---
+++ scripts/build.bat
@@ -0,0 +1,2 @@
+cd C:\dev\Petstore\petstore_sample_service_application.parent
+mvn clean package
---
+++ scripts/delete.bat
@@ -0,0 +1,2 @@
+tibcli login -p T1bc0.Sarah72 -o "EMEA North PreSales" -r "eu-west-1"
+tibcli app delete --force petstore_sample_service_application
