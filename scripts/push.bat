d1f9311e0993f29f1d5b6f2f98e1ed919b2b878e
mmussett <mmussett@tibco.com>
2018-09-19 11:29:27 +0000

    bugs

--- scripts/push.bat
+++ scripts/push.bat
@@ -10,12 +10,12 @@ shift
 shift
 cd %ws%\%app%\target
 copy %ws%\%app%.parent\manifest.json .
-c:\opt\tibcli\tibcli login -p %password% -o %org% -r %reg%
+tibcli login -p %password% -o %org% -r %reg%
 if %ERRORLEVEL% NEQ 0 (
     echo error - unable to login to TIBCO Cloud
     exit
 )
-c:\opt\tibcli\tibcli app push
+tibcli app push
 if %ERRORLEVEL% NEQ 0 (
     echo error - unable to push application to TIBCO Cloud
     exit
